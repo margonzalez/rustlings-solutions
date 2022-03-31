fefe1eb91af3454fff88f4e8225c2584f975237e
Martin Gonzalez <margonzalez.dev@gmail.com>
2022-03-30 23:40:34 -0300

    Almost done. TODO: advance errors

---
+++ exercises/clippy/target/debug/deps/clippy2-f6454fbbab38cd0b.d
@@ -0,0 +1,7 @@
+/Users/mooveit/Workspace/rustlings/exercises/clippy/target/debug/deps/clippy2-f6454fbbab38cd0b.rmeta: clippy2.rs
+
+/Users/mooveit/Workspace/rustlings/exercises/clippy/target/debug/deps/clippy2-f6454fbbab38cd0b.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
